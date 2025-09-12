7d3f166f0fdc72d256f8afaf680aaf1c80c197f6
KDRLaksika <135639511+KDRLaksika@users.noreply.github.com>
2025-09-13 01:11:26 +0530

    Installed some testing library

---
+++ src/components/Hero.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Hero from './Hero';
+
+test('renders Hero component', () => {
+	render(<Hero />);
+	const linkElement = screen.getByText(/hero/i);
+	//expect(linkElement).toBeInTheDocument();
+});
