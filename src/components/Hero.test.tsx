503d310c7582ba22d5ad3fa4ccd1651a9e4baf7c
KDRLaksika <135639511+KDRLaksika@users.noreply.github.com>
2025-09-13 01:28:31 +0530

    Hero.test update

--- src/components/Hero.test.tsx
+++ src/components/Hero.test.tsx
@@ -6,5 +6,5 @@ import Hero from './Hero';
 test('renders Hero component', () => {
 	render(<Hero />);
 	const linkElement = screen.getByText(/hero/i);
-	//expect(linkElement).toBeInTheDocument();
+	expect(linkElement).toBeInTheDocument();
 });
